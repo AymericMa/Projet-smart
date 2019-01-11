77c008f3095673e242df31279934c01a277fb336
Vincent <vincentrue24@gmail.com>
2019-01-11 17:58:14 +0100

    add interruption handler

---
+++ PIC16F_SMART_WEATHER_STATION.X/interupt.h
@@ -0,0 +1,16 @@
+
+#include <xc.h>
+#include "eusart.h"
+#include <stdio.h>
+
+
+
+
+
+
+inline void EnableGlobalinterupts(void);
+inline void DisableGlobalinterupts(void);
+inline void EnablePeripheralInterupts(void);
+inline void DisablePeripheralInterupts(void);
+inline void EnableRXInterupts(void);
+inline void DisableRXInterupts(void);
