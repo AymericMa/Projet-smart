b0a1851067e7d5e5a739b671aa38f99d7529ae69
Vincent <vincentrue24@gmail.com>
2019-01-11 17:57:25 +0100

    add communication's protocols

---
+++ PIC16F_SMART_WEATHER_STATION.X/I2C1.c
@@ -0,0 +1,135 @@
+#include "I2C1.h"
+
+void I2C_Master_Init(const unsigned long c)
+{
+  SSP1CON1 = 0b00101000;            //SSP Module as Master
+  SSP1CON2 = 0;
+  SSP1ADD = (_XTAL_FREQ/(4*c))-1; //Setting Clock Speed
+  SSP1STAT = 0;
+  TRISAbits.TRISA5 = 1;                   //Setting as input as given in datasheet
+  TRISAbits.TRISA4 = 1;                   //Setting as input as given in datasheet
+}
+void I2C_Master_Wait()
+{
+  while ((SSP1STAT & 0x04) || (SSP1CON2 & 0x1F)); //Transmit is in progress
+}
+
+ void I2C_Master_Start()
+{
+  I2C_Master_Wait();
+  SSP1CON2bits.SEN = 1;             //Initiate start condition
+}
+
+ void I2C_Master_ReStart()
+{
+  I2C_Master_Wait();
+  SSP1CON2bits.RSEN = 1;             //Initiate Restart condition
+}
+
+ void I2C_Master_Stop()
+{
+  I2C_Master_Wait();
+  SSP1CON2bits.PEN = 1;           //Initiate stop condition
+}
+
+void I2C_Master_Write(unsigned d)
+{
+  I2C_Master_Wait();
+  SSPBUF = d;         //Write data to SSPBUF
+}
+
+int8_t I2C_Master_Write_V2(unsigned d)
+{
+  I2C_Master_Wait();
+  SSPBUF = d;         //Write data to SSPBUF
+  I2C_Master_Wait();
+  return SSP1CON2bits.ACKSTAT ;
+
+}
+
+unsigned short I2C_Master_Read(unsigned short a)
+{
+  unsigned short temp;
+  I2C_Master_Wait();
+  SSP1CON2bits.RCEN = 1;
+  I2C_Master_Wait();
+  temp = SSPBUF;      //Read data from SSPBUF
+  I2C_Master_Wait();
+  SSP1CON2bits.ACKDT = (a)?0:1;    //Acknowledge bit
+  SSP1CON2bits.ACKEN = 1;          //Acknowledge sequence
+  return temp;
+}
+
+int8_t I2C_Write_data(uint8_t I2C_Slave_8bitAddr,uint8_t *data,uint8_t data_len)
+{
+  uint8_t i ;
+  I2C_Master_Start();  // start condtion
+  if(I2C_Master_Write_V2(I2C_Slave_8bitAddr)) // si 1 --> Acknowledge was not received (datasheet p362)
+  {
+    I2C_Master_Stop(); // on stop
+    return -1 ; // on exit avec erreur
+  }
+  for(i=0;i<data_len;i++) // on envoie les données en fonction de la longeur et on verifie que l'esclave acquitte chaque octet
+  {
+    if(I2C_Master_Write_V2(data[i])) // si 1 --> Acknowledge was not received (datasheet p362)
+    {
+      I2C_Master_Stop(); // on stop
+      return -1 ; // on exit avec erreur
+    }
+  }
+
+ I2C_Master_Stop();
+ return 0 ; //on exit avec succes
+}
+
+
+int8_t I2C_Write_data_then_Read(uint8_t I2C_Slave_7bitAddr,uint8_t *dataIn,uint8_t dataIn_len,uint8_t *dataOut,uint8_t dataOut_len, uint8_t wait_time)
+{
+  uint8_t i ;
+  I2C_Master_Start();  // start condtion
+  if(I2C_Master_Write_V2((I2C_Slave_7bitAddr<<1))) // adresse en ecriture
+  {
+    I2C_Master_Stop(); // on stop
+    return -1 ; // on exit avec erreur
+  }
+  for(i=0;i<dataIn_len;i++) // on envoie les données en fonction de la longeur et on verifie que l'esclave acquitte chaque octet
+  {
+    if(I2C_Master_Write_V2(dataIn[i])) // si 1 --> Acknowledge was not received (datasheet p362)
+    {
+      I2C_Master_Stop(); // on stop
+      return -1 ; // on exit avec erreur
+    }
+  }
+  I2C_Master_Stop();
+  __delay_ms(10); // __delay_ms(wait_time);
+ I2C_Master_Start(); // restart
+
+ if(I2C_Master_Write_V2(((I2C_Slave_7bitAddr<<1)|0x01))) // addresse en lecture
+  {
+    I2C_Master_Stop(); // on stop
+    return -1 ; // on exit avec erreur
+  }
+  for(i=0;i<dataOut_len;i++) // on recoit les données et on acquite sauf la derniere qu'on marque d'un >NACK pour dire qu'on a fini
+  {
+    I2C_Master_Wait();
+    SSP1CON2bits.RCEN = 1;
+    I2C_Master_Wait();
+    dataOut[i] = SSPBUF;      //Read data from SSPBUF
+    I2C_Master_Wait();
+    if(i == (dataOut_len-1)) // si on recoit le dernier on NACK
+    {
+       SSP1CON2bits.ACKDT = 1;    //Acknowledge bit
+       SSP1CON2bits.ACKEN = 1;
+    }
+    else // sinon ACk
+    {
+       SSP1CON2bits.ACKDT = 0;   //Acknowledge bit
+       SSP1CON2bits.ACKEN = 1;
+    }
+
+  }
+  I2C_Master_Stop();
+  return 0 ;
+
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # PIC16_SMART_WEATHER_STATION
+
+## PicKit3 not found ...
+
+If you are having this issue when you try to make and programm device, it mean
+that your account doesn't have the right to access usb for PicKit3... Follow
+those step:
+
+Step #2:
+Create a new group with name "microchip":
+
+`sudo groupadd -g 200 microchip`
+
+Step #3:
+Create a new rules file with editor:
+
+`sudo gedit /etc/udev/rules.d/26-microchip.rules`
+
+Step #4:
+Copy this text to opened file:
+
+`#Read/write access to Microchip devices
+ATTR{idVendor}=="04d8", OWNER:="root", GROUP:="microchip", MODE:="0660"`
+
+Save and close the file.
+
+Step #5:Add yourself to group:
+
+`sudo adduser YOUR_ACCOUNT_NAME microchip`
+
+If you don't now your account name, you can get it with this command:
+`whoami`
+
+Step #6:
+Reboot your computer:
+`sudo reboot`
+
+## Problem with compilation with MPLAX 5 and XC16 2.00
+I get this error when it is using xc8 rather than xc8-cc.  (That's xc8.exe, xc8-cc.exe for Windows users)
+Go to Tools->Options->Embedded->Build Tools and look at the Compiler and Assembler settings for XC8 (v2.00).  If they are for xc8, change them to xc8-cc.  (You might try clicking the "Remove" button for XC8 (v2.00) and then clicking the "Scan for Build Tools" to see if it will come up with xc8-cc on its own.  If not, make the changes manually.)
+
+You may have to change the Standard setting in the Project->XC8 Global Options box to C90 and back to c99 to see the effect of the new setting when you re-compile.  (Maybe even exit the IDE and restart.)
+
+Bottom line: Once I got it to use xc8-cc, things went smoothly.
